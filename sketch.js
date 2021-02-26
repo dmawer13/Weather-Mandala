76061c9bab91518f795afc9edeca459b49fcbb83
dmawer <dmawer@buffalo.edu>
2021-02-26 08:10:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var weather;
 var cityInput = "Buffalo";
 
 function askWeather() {
-    loadJSON('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&APPID=40223864a797a6dcafe07148fc944088&units=imperial', gotData);
+    loadJSON('api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&APPID=40223864a797a6dcafe07148fc944088&units=imperial', gotData);
 }
 
 function setup() {
